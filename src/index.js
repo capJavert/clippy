65707d749908ee792d92605790d4dabc12734e57
Ante Barić <ante.baric@burza.hr>
2018-07-26 11:13:53 +0200

    update(website):  - adjusted poster image animations  - code cleanup  - removed fucking jquery

--- src/index.js
+++ src/index.js
@@ -2,43 +2,51 @@ import './assets/scss/main.scss'
 import './assets/js/plugins.js'
 import {BrowserEnum, whichBrowser} from './assets/js/helpers/which-browser.js'
 
-$(document).ready(function() {
-  var browserPlatform = whichBrowser()
+window.onload = () => {
+  const browserPlatform = whichBrowser()
 
   if ([BrowserEnum.chrome, BrowserEnum.firefox, BrowserEnum.opera].indexOf(browserPlatform) > -1) {
-    $('.Button-download').hide()
+    document.querySelectorAll('.Button-download').forEach(element => {
+      element.style.display = 'none'
+    })
 
     switch(browserPlatform) {
     case BrowserEnum.chrome:
-      $('.Button-download-chrome').show()
+      document.querySelector('.Button-download-chrome').style.display = 'inline-block'
       break
     case BrowserEnum.firefox:
-      $('.Button-download-firefox').show()
+      document.querySelector('.Button-download-firefox').style.display = 'inline-block'
       break
     case BrowserEnum.opera:
-      $('.Button-download-opera').show()
+      document.querySelector('.Button-download-opera').style.display = 'inline-block'
       break
     }
   }
 
-  // logo animation demo
-  // TODO clear this up
-  var stop1Elem = document.getElementById('stop1');
-  var stop2Elem = document.getElementById('stop2');
-  var stop3Elem = document.getElementById('stop3');
+  animatePosterLogo();
+}
 
-  // var stop3Elem = document.getElementById('stop3');
-  // var stop4Elem = document.getElementById('stop4');
+function animatePosterLogo() {
+  const foregroundCircle = document.getElementById('ForegroundCircle');
+  const backgroundCircle = document.getElementById('BackgroundCircle')
+  const gradient1 = {
+    1: document.getElementById('Gradient1-1'),
+    2: document.getElementById('Gradient1-2'),
+    3: document.getElementById('Gradient1-3')
+  }
 
   setInterval(function() {
-    var stop1 = [Math.floor(Math.random() * 15) + 45, Math.floor(Math.random() * 16) + 76, Math.floor(Math.random() * 13) + 133];
-    var stop2 = [Math.floor(Math.random() * 14) + 74, Math.floor(Math.random() * 16) + 126, Math.floor(Math.random() * 12) + 92];
-    var stop3 = [Math.floor(Math.random() * 18) + 88, Math.floor(Math.random() * 18)+ 158, Math.floor(Math.random() * 15)+ 175]
+    const color = {
+      1: [Math.floor(Math.random() * 30) + 48, Math.floor(Math.random() * 30) + 158, Math.floor(Math.random() * 10) + 242],
+      2: [Math.floor(Math.random() * 30) + 47, Math.floor(Math.random() * 10) + 239, Math.floor(Math.random() * 20) + 117],
+      3: [Math.floor(Math.random() * 30) + 35, Math.floor(Math.random() * 30)+ 92, Math.floor(Math.random() * 10)+ 237]
+    }
 
-    // stop1Elem.style.stopColor = stop4Elem.style.stopColor = 'rgb(' + stop1.toString() + ')';
-    // stop2Elem.style.stopColor = stop3Elem.style.stopColor = 'rgb(' + stop2.toString() + ')';
-    stop1Elem.style.stopColor = 'rgb(' + stop1.toString() + ')';
-    stop2Elem.style.stopColor = 'rgb(' + stop2.toString() + ')';
-    stop3Elem.style.stopColor = 'rgb(' + stop3.toString() + ')';
-  }, 300);
-})
+    TweenLite.to(foregroundCircle, .8, {rotation: Math.floor(Math.random() * 50) + 60, transformOrigin: '50% 50%'})
+    TweenLite.to(backgroundCircle, .8, {rotation: Math.floor(Math.random() * 360), transformOrigin: '50% 50%'})
+
+    TweenLite.to(gradient1[1], .4, {stopColor: 'rgb(' + color[1].toString() + ')'})
+    TweenLite.to(gradient1[2], .4, {stopColor: 'rgb(' + color[2].toString() + ')'})
+    TweenLite.to(gradient1[3], .4, {stopColor: 'rgb(' + color[3].toString() + ')'})
+  }, 600);
+}
