3de47eedb9dc942ed8b7d154a4b5c9a529433b98
capJavert <ante.baric3@gmail.com>
2019-08-25 22:35:24 +0200

    feature(linter): added test groups, adjusted test command

--- __tests__/state.test.js
+++ __tests__/state.test.js
@@ -71,100 +71,104 @@ afterEach(() => {
     jest.resetModules();
 })
 
-test('isActive listener', () => {
-    expect.assertions(2)
+describe('State listener responds to messages', () => {
+    test('isActive listener', () => {
+        expect.assertions(2)
 
-    browser.runtime.onMessage.listener({ name: 'isActive' }, {}, (response) => {
-        expect(response).toEqual({
-            name: 'isActive',
+        browser.runtime.onMessage.listener({ name: 'isActive' }, {}, (response) => {
+            expect(response).toEqual({
+                name: 'isActive',
+                value: true
+            })
+        })
+
+        window.settings.isActive = false
+
+        browser.runtime.onMessage.listener({ name: 'isActive' }, {}, (response) => {
+            expect(response).toEqual({
+                name: 'isActive',
+                value: false
+            })
+        })
+    })
+
+    test('comments listener', () => {
+        function XMLHttpRequest() {
+            return {
+                send() {
+                    if (typeof this.onreadystatechange() === 'function') {
+                        this.onreadystatechange()
+                    }
+                },
+                open() {
+                    this.readyState = 4
+                    this.status = 200
+                    this.response = JSON.stringify(dictionary)
+                }
+            }
+        }
+        global.XMLHttpRequest = XMLHttpRequest
+
+        browser.runtime.onMessage.listener({ name: 'comments' })
+
+        expect(window.settings.comments).toEqual(dictionary)
+    })
+
+    test('idle listener', () => {
+        expect.assertions(4)
+        jest.useFakeTimers()
+        let lastId
+        let lastMessage
+        browser.tabs.sendMessage = (id, message) => {
+            if (message.name === 'animate') {
+                lastId = id
+                lastMessage = message
+            }
+        }
+
+        window.settings.isActive = false
+        browser.runtime.onMessage.listener({ name: 'idle' })
+        jest.runAllTimers()
+
+        expect(lastId).toBeUndefined()
+        expect(lastMessage).toBeUndefined()
+
+        window.settings.isActive = true
+        browser.runtime.onMessage.listener({ name: 'idle' })
+        jest.runAllTimers()
+
+        expect(lastId).toEqual(1)
+        expect(lastMessage).toEqual({
+            name: 'animate',
             value: true
         })
     })
-
-    window.settings.isActive = false
-
-    browser.runtime.onMessage.listener({ name: 'isActive' }, {}, (response) => {
-        expect(response).toEqual({
-            name: 'isActive',
-            value: false
-        })
-    })
 })
 
-test('comments listener', () => {
-    function XMLHttpRequest() {
-        return {
-            send() {
-                if (typeof this.onreadystatechange() === 'function') {
-                    this.onreadystatechange()
-                }
-            },
-            open() {
-                this.readyState = 4
-                this.status = 200
-                this.response = JSON.stringify(dictionary)
-            }
+describe('Toolbar controls are working', () => {
+    it('should toggle button icons on click', () => {
+        expect.assertions(tabs.length * 2)
+
+        browser.browserAction.setIcon = (payload) => {
+            expect(payload.path).toBeDefined()
+            expect(payload.tabId).toBeDefined()
         }
-    }
-    global.XMLHttpRequest = XMLHttpRequest
 
-    browser.runtime.onMessage.listener({ name: 'comments' })
-
-    expect(window.settings.comments).toEqual(dictionary)
-})
-
-test('idle listener', () => {
-    expect.assertions(4)
-    jest.useFakeTimers()
-    let lastId
-    let lastMessage
-    browser.tabs.sendMessage = (id, message) => {
-        if (message.name === 'animate') {
-            lastId = id
-            lastMessage = message
-        }
-    }
-
-    window.settings.isActive = false
-    browser.runtime.onMessage.listener({ name: 'idle' })
-    jest.runAllTimers()
-
-    expect(lastId).toBeUndefined()
-    expect(lastMessage).toBeUndefined()
-
-    window.settings.isActive = true
-    browser.runtime.onMessage.listener({ name: 'idle' })
-    jest.runAllTimers()
-
-    expect(lastId).toEqual(1)
-    expect(lastMessage).toEqual({
-        name: 'animate',
-        value: true
+        browser.browserAction.onClicked.listener()
     })
-})
 
-it('should toggle button icons on click', () => {
-    expect.assertions(tabs.length * 2)
+    it('should toggle isActive status on click', () => {
+        expect.assertions(tabs.length * 2)
+        window.settings.isActive = true
 
-    browser.browserAction.setIcon = (payload) => {
-        expect(payload.path).toBeDefined()
-        expect(payload.tabId).toBeDefined()
-    }
+        browser.tabs.sendMessage = (tabId, message) => {
+            expect(tabId).toBeDefined()
+            expect(message).toEqual({
+                name: 'isActive',
+                value: false
+            })
+        }
 
-    browser.browserAction.onClicked.listener()
-})
-
-it('should toggle isActive status on click', () => {
-    expect.assertions(tabs.length * 2)
-    window.settings.isActive = true
-
-    browser.tabs.sendMessage = (tabId, message) => {
-        expect(tabId).toBeDefined()
-        expect(message).toEqual({
-            name: 'isActive',
-            value: false
-        })
-    }
-
-    browser.browserAction.onClicked.listener()
+        browser.browserAction.onClicked.listener()
+    })
 })
